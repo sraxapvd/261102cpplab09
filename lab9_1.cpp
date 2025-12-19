4e01d9249ad481488a34c9066606a0096b486c35
Rada <sirada_dee@cmu.ac.th>
2025-12-19 14:50:32 +0700

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	double initial , rate , pay ;
+
+	cout << "Enter initial loan: ";
+	cin >> initial;
+	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
+
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -18,16 +21,34 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+    cout << fixed << setprecision(2); 
 	
+	double prevBalance = initial;
+	int year = 1;
+
+	while (prevBalance > 0) {
+		double interest = prevBalance * rate / 100.0;
+		double total = prevBalance + interest;
+
+		double payment;
+		if (total < pay)
+			payment = total;
+		else
+			payment = pay;
+		
+		double newBalance = total - payment;
+
+		cout << setw(13) << left << year
+		     << setw(13) << left << prevBalance
+		     << setw(13) << left << interest
+		     << setw(13) << left << total
+		     << setw(13) << left << payment
+		     << setw(13) << left << newBalance
+		     << "\n";
+
+		prevBalance = newBalance;
+		year++;
+	}
+
 	return 0;
 }
